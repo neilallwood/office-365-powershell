bd8a3fd4fc50e3384ef544177a21296b50cf463c
neilallwood <16964133+neilallwood@users.noreply.github.com>
2018-08-07 16:32:47 +0100

    Bug fixes

--- office-reassign-license.ps1
+++ office-reassign-license.ps1
@@ -20,26 +20,30 @@ $CSVPath = '~\Desktop\users.csv'
 # Get the SKU's that are currently subscribed to on the tennant
 Get-MsolAccountSku
 
-$SKUTo = Read-Host -Prompt "Enter the SKU you wish to enable"
-$SKUFrom = Read-Host -Prompt "Enter the SKU you wish to disable"
+# Specify the license you with to go to and the one you wish to remove
+Write-Host "Enter the SKU you wish to enable. Copy and paste the complete SKU. For example, tennantname:product:"
+$SKUTo = Read-Host
+$SKUToListLicenseOptions = $SKUTo.Split(':')[-1]
+Write-Host "Enter the SKU you wish to disable. Copy and paste the complete SKU. For example, tennantname:product:"
+$SKUFrom = Read-Host
 
 # Show the licenseoptions for the SKU you are changing to
-$ServicePlans = Get-MsolAccountSku | Where {$_.SkuPartNumber -eq $SKUTo}
+$ServicePlans = Get-MsolAccountSku | Where {$_.SkuPartNumber -eq $SKUToListLicenseOptions}
 $ServicePlans.ServiceStatus
 
 # Specify the name(s) of the license options you want to disable. For multiple, use a comma with no spaces between each one.
-$DisableServicePlan = Read-Host -Prompt "Enter the name for the service plans to disable for the SKU you are changing to. If multiple, seperate by using a comma between each one"
-$NewLicenseOption = New-MsolLicenseOptions -AccountSkuId $SKUTo -DisabledPlans $DisableServicePlan
+# For example: $NewLicenseOption = New-MsolLicenseOptions -AccountSkuId $SKUTo -DisabledPlans SWAY,Deskless,YAMMER_ENTERPRISE
+$LicenseOption = New-MsolLicenseOptions -AccountSkuId $SKUTo -DisabledPlans <replace-with-license-option(s)>
 
-# Get list of users with SKUFrom
+# Get list of users with $SKUFrom and export them to a CSV file
 Get-MsolUser -All `
     | Where {$_.isLicensed -eq “TRUE” -and $_.Licenses.AccountSKUID -eq $SKUFrom} `
     | Export-Csv -Path $CSVPath
 
 # Assign the licenses to the user(s), disable any unwanted service plans and disable the previous subscription
-$users = import-csv $CSVPath -delimiter ","
-foreach ($user in $users)
+$Users = import-csv $CSVPath -delimiter ","
+foreach ($User in $Users)
 {
-    $upn=$user.UserPrincipalName
-    Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKUTo -LicenseOptions $NewlicenseOption -RemoveLicenses $SKUFrom
+    $UserName=$User.UserPrincipalName
+    Set-MsolUserLicense -UserPrincipalName $UserName -AddLicenses $SKUTo -LicenseOptions $licenseOption -RemoveLicenses $SKUFrom
 }
